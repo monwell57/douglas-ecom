76d070e540d4b5b27dc107bccef75b6e3ab670ff
Gregory Peters <greg.monwell57@gmail.com>
2024-06-01 14:28:13 -0700

    Add Newest Section Pt 1

--- src/app/(shop)/shop/page.jsx
+++ src/app/(shop)/shop/page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import HeaderImage from "./components/HeaderImage";
 
+export const revalidate = 30;
+
 const ShopPage = () => {
   return (
     <div className="min-h-screen bg-gray-100   text-gray-900">
