dafbcc3d3492b21a9d7ba149699c742e2128441a
Gregory Peters <greg.monwell57@gmail.com>
2024-06-10 10:53:31 -0700

    Fix Stripe Success and ErroR  URL Final

--- src/app/(shop)/shop/components/Providers.jsx
+++ src/app/(shop)/shop/components/Providers.jsx
@@ -8,8 +8,8 @@ function CartProvider({ children }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY}
-      successUrl="https://complete-artist-site-9ab82jvit-monwell57s-projects.vercel.app/shop/stripe/success"
-      cancelUrl="https://complete-artist-site-9ab82jvit-monwell57s-projects.vercel.app/shop/stripe/error"
+      successUrl={process.env.STRIPE_SUCCESS_URL}
+      cancelUrl={process.env.STRIPE_ERROR_URL}
       currency="USD"
       billingAddressCollection={true}
       shouldPersist={true}
