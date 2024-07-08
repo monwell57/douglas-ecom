2a3f9e1a984b44ed2dcd7dcf50e392db35f4ebf9
Gregory Peters <greg.monwell57@gmail.com>
2024-07-08 09:26:22 -0700

    Add Order Date

--- src/app/(shop)/shop/components/Order.js
+++ src/app/(shop)/shop/components/Order.js
@@ -14,12 +14,13 @@ export default async function Order() {
   return (
     <div className="max-w-3xl mx-auto mt-20">
       <h1 className="text-3xl text-center font-semibold text-[#5B20B6] mb-6">
-        Your Orders Page
+        Your Order History
       </h1>
 
       <table className="w-full border-collapse">
         <thead>
           <tr className="text-[#5B20B6] border-b border-gray-200">
+            <th className="py-2 px-4">Order Date</th>
             <th className="py-2 px-4">Product</th>
             <th className="py-2 px-4">Quantity</th>
             <th className="py-2 px-4">Price</th>
@@ -28,30 +29,40 @@ export default async function Order() {
           </tr>
         </thead>
         <tbody>
-          {fetchedOrders.map((order) => (
-            <tr
-              key={order._id}
-              className="hover:bg-gray-50 text-center border-b border-gray-300 text-[#5B20B6]"
-            >
-              <td className="py-2 px-4 flex items-center">{order.name}</td>
-              <td className="py-2 px-4">{order.qty}</td>
-              <td className="py-2 px-4">${order.price}</td>
-              <td className="py-2 px-4">
-                {order.paid ? (
-                  <span className="text-green-500">Paid</span>
-                ) : (
-                  <span className="text-red-500">Unpaid</span>
-                )}
-              </td>
-              <td className="py-2 px-4">
-                {order.delivered ? (
-                  <span className="text-green-500">Delivered</span>
-                ) : (
-                  <span className="text-red-500">In transit</span>
-                )}
-              </td>
-            </tr>
-          ))}
+          {fetchedOrders.map((order) => {
+            const orderDate = new Date(order.createdAt);
+            const formattedDate = !isNaN(orderDate)
+              ? orderDate.toLocaleDateString()
+              : "Invalid Date";
+
+            return (
+              <tr
+                key={order._id}
+                className="hover:bg-gray-50 text-center border-b border-gray-300 text-[#5B20B6]"
+              >
+                <td className="py-2 px-4">
+                  <td className="py-2 px-4">{formattedDate}</td>
+                </td>
+                <td className="py-2 px-4 flex items-center">{order.name}</td>
+                <td className="py-2 px-4">{order.qty}</td>
+                <td className="py-2 px-4">${order.price}</td>
+                <td className="py-2 px-4">
+                  {order.paid ? (
+                    <span className="text-green-500">Paid</span>
+                  ) : (
+                    <span className="text-red-500">Unpaid</span>
+                  )}
+                </td>
+                <td className="py-2 px-4">
+                  {order.delivered ? (
+                    <span className="text-green-500">Delivered</span>
+                  ) : (
+                    <span className="text-red-500">In transit</span>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
